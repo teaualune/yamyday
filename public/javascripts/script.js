fe0fbe4d96ff7d26c27b866e8c6952be651cf89e
teaualune <teaualune@gmail.com>
2013-11-02 23:30:49 +0800

    merge frontend

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,40 @@
 (function (A, $) {
-    // var app = A.module('yamyday', []);
+    var sectionNewsSchema = function () {
+            return {
+                title: null,
+                subTitle: null,
+                imgUrl: null,
+                content: null,
+                newsID: null,
+            };
+        },
+
+        detailNewsSchema = function () {
+            return {
+                newsTitle: null,
+                imgUrl: null,
+                content: null,
+                newsID: null,
+                shares: null
+            };
+        },
+
+        mergeIntoNews = function (news, S, name) {
+            var link, s, i;
+            for (link in S) {
+                if (S.hasOwnProperty(link)) {
+                    s = S[link];
+                    for (i = 0; i < news.length; i = i + 1) {
+                        if (news[i]._id === link) {
+                            news[i][name] = s;
+                            break;
+                        }
+                    }
+                }
+            }
+        };
+
+    window.app = A.module('yamyday', []);
     // app.factory('newspaperStack', function () {
     //     var _c = {
     //             getPage: function () {
@@ -57,14 +92,115 @@
     //     }
     // });
 
-    $.ajax({
-        type: 'GET',
-        url: '/ajax/main',
-        dataType: 'json'
-    }).done(function (res) {
-        console.log(res);
-    }).fail(function (err) {
-        console.log(err);
+    app.factory('newsModel', function () {
+        var news,
+            scores,
+            shares,
+            headline = {
+                title: null,
+                subTitle: null,
+                imgUrl: null,
+                newsID: null,
+                secondTitle: null,
+                thirdTitle: null
+            },
+            one = {
+                content: null,
+                shares: null
+            },
+            two = [
+                sectionNewsSchema(),
+                sectionNewsSchema(),
+                sectionNewsSchema(),
+                sectionNewsSchema()
+            ],
+            three = [
+                sectionNewsSchema(),
+                sectionNewsSchema(),
+                sectionNewsSchema(),
+                sectionNewsSchema(),
+                sectionNewsSchema()
+            ];
+        return {
+            init: function (data) {
+                var n = 0,
+                    i;
+
+                news = data.news;
+                scores = data.scores;
+                shares = data.shares;
+                mergeIntoNews(news, scores, 'score');
+                mergeIntoNews(news, shares, 'shares');
+
+                if (news.length === 0) {
+                    console.log('...');
+                }
+                if (news.length > 0) { // at least 1
+                    // who says it: data.news[0].shares[0].from.name
+                    headline.title = data.news[0].shares[0].message;
+                    headline.subTitle = data.news[0].title;
+                    headline.imgUrl = data.news[0].imgUrl;
+                    headline.newsID = data.news[0]._id;
+                    one.content = data.news[0].content;
+                    one.shares = data.news[0].shares;
+                }
+                if (news.length > 1) { // at least 2
+                    n = Math.min(data.news.length, 5);
+                    for (i = 1; i < n; i = i + 1) {
+                        // who says it: data.news[i].shares[0].from.name
+                        two[i - 1].title = data.news[i].shares[0].message;
+                        two[i - 1].subTitle = data.news[i].title;
+                        two[i - 1].imgUrl = data.news[i].imgUrl;
+                        two[i - 1].content = data.news[i].content;
+                        two[i - 1].newsID = data.news[i]._id;
+                    }
+                    headline.secondTitle = data.news[1].title;
+                    headline.thirdTitle = (data.news[2]) ? data.news[2].title : null;
+                }
+                if (news.length > 5) { // at least 6
+                    n = Math.min(data.news.length, 10);
+                    for (i = 5; i < n; i = i + 1) {
+                        // who says it: data.news[i].shares[0].from.name
+                        three[i - 5].title = data.news[i].shares[0].message;
+                        three[i - 5].subTitle = data.news[i].title;
+                        three[i - 5].imgUrl = data.news[i].imgUrl;
+                        three[i - 5].content = data.news[i].content;
+                        three[i - 5].newsID = data.news[i]._id;
+                    }
+                }
+            },
+            headline: headline,
+            one: one,
+            two: two,
+            three: three,
+            detailNews: null // pointer to one of the news
+        };
     });
+
+    app.controller('MainCtrl', [
+        '$scope',
+        '$http',
+        'newsModel',
+        function (s, h, m) {
+            s.headline = m.headline;
+            s.one = m.one;
+            s.two = m.two;
+            s.three = m.three;
+            h.get('/ajax/main').success(function (data) {
+                console.log(data);
+                $('.pages').removeClass('invisible');
+                $('.loading').addClass('hidden');
+                m.init(data);
+            });
+        }
+    ]);
+
+    app.controller('DetailPageCtrl', [
+        '$scope',
+        'newsModel',
+        function (s, m) {
+            s.detailNews = m.detailNews;
+        }
+    ]);
 
 }(angular, jQuery));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function (req, res) {
+    res.locals.sectionHelper = function (partition, section, i) {
+        return text = [
+            '<div class="section s-1-' + partition + '"><img src="{{ ',
+            section + '[' + i + '].imgUrl }}" /><h3>{{ ',
+            section + '[' + i + '].title }}</h3><h4>{{ ',
+            section + '[' + i + '].subTitle }}</h4><span class="more">more</span><span class="store"></span></div>'
+        ].join('');
+    };
     res.render('index', { title: req.session.fbid });
 };
